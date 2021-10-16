f1ef95029e39a5cd718970b22c518c5a06e56a80
JuliaMathias <juliamss@gmail.com>
2021-10-16 10:34:34 -0300

    add comment length validation

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :movie_id, uniqueness: { scope: :list_id, message: 'This movie has already been saved!' }
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id, message: 'is already in the list' }
 end
