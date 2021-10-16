c4f473cf0bb21c5671ccfaa1233adcc1292b388d
JuliaMathias <juliamss@gmail.com>
2021-10-16 10:31:45 -0300

    rename saved_movies to bookmarks

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,16 @@ ActiveRecord::Schema.define(version: 2021_10_16_130633) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookmarks", force: :cascade do |t|
+    t.text "comment"
+    t.bigint "movie_id", null: false
+    t.bigint "list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["list_id"], name: "index_bookmarks_on_list_id"
+    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
+  end
+
   create_table "lists", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -30,16 +40,6 @@ ActiveRecord::Schema.define(version: 2021_10_16_130633) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "saved_movies", force: :cascade do |t|
-    t.text "comment"
-    t.bigint "movie_id", null: false
-    t.bigint "list_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["list_id"], name: "index_saved_movies_on_list_id"
-    t.index ["movie_id"], name: "index_saved_movies_on_movie_id"
-  end
-
-  add_foreign_key "saved_movies", "lists"
-  add_foreign_key "saved_movies", "movies"
+  add_foreign_key "bookmarks", "lists"
+  add_foreign_key "bookmarks", "movies"
 end
